97f68a5c0d79598644b15803de69d27a2a30aac2
Marco <160401397+carfizzi@users.noreply.github.com>
2024-04-11 19:17:08 +0200

    Created db service
    Imported Dexie;
    Implemented DbService for CRUD operations;

---
+++ src/app/models/database/order.ts
@@ -0,0 +1,5 @@
+export interface Order {
+    id?: number;
+    packaging_code_ext: string,
+    insertion_date: Date
+}
---
+++ src/app/services/db.ts
@@ -0,0 +1,59 @@
+import { Injectable } from "@angular/core";
+import Dexie from "dexie";
+import { from, Observable } from "rxjs";
+import { Order } from "../models/database/order";
+import { Packaging } from "../models/database/packaging";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DBService extends Dexie {
+  public packagings: Dexie.Table<Packaging, string>;
+  public orders: Dexie.Table<Order, number>;
+
+  constructor() {
+    super('warehouse-db');
+    this.version(1).stores({
+      packagings: 'code, label',
+      orders: '++id, packaging_code_ext, insertion_date'
+    });
+
+    this.packagings = this.table('packagings');
+    this.orders = this.table('orders');
+  }
+
+  // CRUD methods for Packagings table
+  public addPackaging(packaging: Packaging): Observable<string> {
+    return from(this.packagings.add(packaging));
+  }
+
+  public getPackaging(code: string): Observable<Packaging | undefined> {
+    return from(this.packagings.get(code));
+  }
+
+  public updatePackaging(code: string, changes: Partial<Packaging>): Observable<number> {
+    return from(this.packagings.update(code, changes));
+  }
+
+  public deletePackaging(code: string): Observable<void> {
+    return from(this.packagings.delete(code));
+  }
+
+  // CRUD methods for Orders table
+  public addOrder(order: Order): Observable<number> {
+    return from(this.orders.add(order));
+  }
+
+  public getOrder(id: number): Observable<Order | undefined> {
+    return from(this.orders.get(id));
+  }
+
+  public updateOrder(id: number, changes: Partial<Order>): Observable<number> {
+    return from(this.orders.update(id, changes));
+  }
+
+  public deleteOrder(id: number): Observable<void> {
+    return from(this.orders.delete(id));
+  }
+}
+
